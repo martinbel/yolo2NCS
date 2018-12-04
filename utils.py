dd738dcb19ce765dfe73fdf45ac5a2361e466ced
m4rbel <m4rbel@gmail.com>
2018-12-04 11:07:51 -0300

    changes to NMS

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ import copy
 import cv2
 import sys
 import shutil
-
+from scipy.special import expit
 
 class BoundBox:
     def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
@@ -52,7 +52,7 @@ class WeightReader:
 
 
 def bbox_iou(box1, box2):
-    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_w = _interval_overlap([box1.xmin,     box1.xmax], [box2.xmin, box2.xmax])
     intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
 
     intersect = intersect_w * intersect_h
@@ -83,74 +83,118 @@ def draw_boxes(image, boxes, labels):
         xmax = int(box.xmax*image_w)
         ymax = int(box.ymax*image_h)
 
-        line_width_factor = int(min(image_h,image_w)*0.005)
-        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), colors[box.get_label()], line_width_factor*2)
+        line_width_factor = int(min(image_h,image_w) * 0.005)
+        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), colors[box.get_label()], line_width_factor * 2)
         cv2.putText(image,
-                    "{} {:.3f}".format(labels[box.get_label()],box.get_score()),
+                    "{:.2f} {}".format(box.get_score(), labels[box.get_label()]),
                     (xmin, ymin - line_width_factor * 3),
                     cv2.FONT_HERSHEY_PLAIN,
-                    2e-3 * min(image_h,image_w),
-                    (0,255,0), line_width_factor)
+                    3e-3 * min(image_h, image_w),
+                    (255,255,0), line_width_factor)
 
     return image
 
+# Malisiewicz et al.
+def non_max_suppression(boxes, overlapThresh=0.5):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
 
-def decode_netout(netout, anchors, nb_class, obj_threshold=0.52, nms_threshold=0.5):
+    boxes_array = np.array([(x.xmin, x.ymin, x.xmax, x.ymax) for x in boxes])
+
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes_array.dtype.kind == "i":
+        boxes_array = boxes_array.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes_array[:,0]
+    y1 = boxes_array[:,1]
+    x2 = boxes_array[:,2]
+    y2 = boxes_array[:,3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
+
+    # return only the bounding boxes that were picked using the
+    selected_bbox = [boxes[p] for p in pick]
+
+    return selected_bbox
+
+
+def decode_netout(netout, anchors, nb_class, obj_threshold=0.50, nms_threshold=0.5):
     grid_h, grid_w, nb_box = netout.shape[:3]
 
-    # print("In netout{}".format(netout.shape))
-    #print(netout.min(), netout.max(), netout.mean())
-
     boxes = []
-
-    # decode the output by the network
-    netout[..., 4]  = _sigmoid(netout[..., 4])
+    ### decode the output by the network
+    # 4th index has the object probability, we take the sigmoid of it.
+    netout[..., 4]  = expit(netout[..., 4])
+    # Prob object exists (index 4) * Sofmax(each of presence of class, index 5:)
     netout[..., 5:] = netout[..., 4][..., np.newaxis] * _softmax(netout[..., 5:])
     netout[..., 5:] *= netout[..., 5:] > obj_threshold
-
-    #print("Out netout{}".format(netout.mean()))
-    #print(netout.min(), netout.max(), netout.mean())
 
     for row in range(grid_h):
         for col in range(grid_w):
             for b in range(nb_box):
                 # from 4th element onwards are confidence and class classes
                 classes = netout[row, col, b, 5:]
-
                 if np.sum(classes) > 0:
                     # first 4 elements are x, y, w, and h
                     x, y, w, h = netout[row,col,b,:4]
-
-                    x = (col + _sigmoid(x)) / grid_w # center position, unit: image width
-                    y = (row + _sigmoid(y)) / grid_h # center position, unit: image height
+                    x = (col + expit(x)) / grid_w # center position, unit: image width
+                    y = (row + expit(y)) / grid_h # center position, unit: image height
                     w = anchors[2 * b + 0] * np.exp(w) / grid_w # unit: image width
                     h = anchors[2 * b + 1] * np.exp(h) / grid_h # unit: image height
                     confidence = netout[row,col,b,4]
-
                     box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, confidence, classes)
-
                     boxes.append(box)
-
-    # suppress non-maximal boxes
-    for c in range(nb_class):
-        sorted_indices = list(reversed(np.argsort([box.classes[c] for box in boxes])))
-
-        for i in range(len(sorted_indices)):
-            index_i = sorted_indices[i]
-
-            if boxes[index_i].classes[c] == 0:
-                continue
-            else:
-                for j in range(i+1, len(sorted_indices)):
-                    index_j = sorted_indices[j]
-
-                    if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_threshold:
-                        boxes[index_i].classes[c] = 0
 
     # remove the boxes which are less likely than a obj_threshold
     boxes = [box for box in boxes if box.get_score() > obj_threshold]
+    # TODO: add support for more classes
+    # suppress non-maximal boxes
+#    for c in range(nb_class):
+        #sorted_indices = list(reversed(np.argsort([box.classes[c] for box in boxes])))
+#        box =
+    selected = non_max_suppression(boxes, overlapThresh=nms_threshold)
 
-    return boxes
+    if len(selected) == 0:
+        selected = []
+
+    return selected
 
 
 def compute_overlap(a, b):
@@ -259,6 +303,8 @@ def import_feature_extractor(backend, input_size):
         feature_extractor = MobileNetFeature(input_size)
     elif backend == 'Full Yolo':
         feature_extractor = FullYoloFeature(input_size)
+    elif backend == 'Tiny Yolo Voc':
+        feature_extractor = TinyYoloVocFeature(input_size)
     elif backend == 'Tiny Yolo':
         feature_extractor = TinyYoloFeature(input_size)
     elif backend == 'VGG16':
@@ -316,20 +362,19 @@ def get_session():
     return tf.Session(config=config)
 
 def create_backup(config):
-
     backup_folder = config['backup']['backup_path']
     prefix = config['backup']['backup_prefix']
-    backup_id = datetime.now().strftime('%Y%m%d%H%M%S')
+    backup_id = datetime.now().strftime('%d%H%M')
     train_folder_name = "_".join([prefix,backup_id])
     path = os.path.join(backup_folder,train_folder_name)
     if os.path.isdir(path) :
         shutil.rmtree(path)
     os.makedirs(path)
 
-    shutil.copytree(os.path.dirname(os.path.realpath(__file__)),os.path.join(path,"Keras-yolo2"), ignore=shutil.ignore_patterns(".git"))
+    shutil.copytree(os.path.dirname(os.path.realpath(__file__)), os.path.join(path,"Keras-yolo2"), ignore=shutil.ignore_patterns(".git"))
     readme_message = ""
-    while(readme_message == ""):
-        readme_message = input("Insert a comment about this training: ")
+    #while(readme_message == ""):
+    #    readme_message = input("Insert a comment about this training: ")
     with open(os.path.join(path,"readme.txt"),'w') as readme_file:
         readme_file.write(readme_message)
 
@@ -343,3 +388,74 @@ def create_backup(config):
         config['train']['tensorboard_log_dir'] = log_name
 
     return config
+
+
+
+
+
+def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6):
+    """Filters YOLO boxes by thresholding on object and class confidence.
+
+    Arguments:
+    box_confidence -- tensor of shape (grid, grid, nb_anchor, class)
+    boxes -- tensor of shape (grid, grid, nb_anchor, nb_box)
+    box_class_probs -- tensor of shape (grid, grid, nb_anchor, nb_classes)
+    threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
+
+    Returns:
+    scores -- tensor of shape (None,), containing the class probability score for selected boxes
+    boxes -- tensor of shape (None, 4), containing (b_x, b_y, b_h, b_w) coordinates of selected boxes
+    classes -- tensor of shape (None,), containing the index of the class detected by the selected boxes
+
+    Note: "None" is here because you don't know the exact number of selected boxes, as it depends on the threshold.
+    For example, the actual output size of scores would be (10,) if there are 10 boxes.
+    """
+
+    # Step 1: Compute box scores
+    box_scores = box_confidence * box_class_probs # 19x19x80
+
+    # Step 2: Find the box_classes thanks to the max box_scores, keep track of the corresponding score
+    box_classes = np.argmax(box_scores, axis = -1)  # 19x19x5x1  (1 class index)
+    box_class_scores = np.max(box_scores, axis = -1)  # 19x19x5x1 (1 class score)
+
+    # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
+    # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
+    filtering_mask = box_class_scores >= threshold
+
+    # Step 4: Apply the mask to scores, boxes and classes
+    scores = box_class_scores[filtering_mask]
+    boxes = boxes[filtering_mask]
+    classes = box_classes[filtering_mask]
+
+    return scores, boxes, classes
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    ### START CODE HERE ### (≈ 5 lines)
+    xi1 = np.max([box1[0], box2[0]])
+    yi1 = np.max([box1[1], box2[1]])
+    xi2 = np.min([box1[2], box2[2]])
+    yi2 = np.min([box1[3],box2[3]])
+    inter_area = max((yi2-yi1),0) * max((xi2-xi1),0)
+    ### END CODE HERE ###
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    ### START CODE HERE ### (≈ 3 lines)
+    box1_area = (box1[3]-box1[1]) * (box1[2] - box1[0])
+    box2_area = (box2[3]-box2[1]) * (box2[2] - box2[0])
+    union_area = box1_area + box2_area - inter_area
+    ### END CODE HERE ###
+
+    # compute the IoU
+    ### START CODE HERE ### (≈ 1 line)
+    iou = inter_area/union_area
+    ### END CODE HERE ###
+
+    return iou
